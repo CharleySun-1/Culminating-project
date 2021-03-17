de8b792de2bd31da2de3a3aa041d7d45bac70790
CharleySun <Ysun@lcs.on.ca>
2021-03-17 12:11:23 -0400

    Initial Commit

---
+++ Culminating project/Culminating_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_projectApp.swift
+//  Culminating project
+//
+//  Created by Charley Sun on 2021-03-17.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
